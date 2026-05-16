2d32ecf14302012a528ff6fd39941ab753a729b4
maripasa <asafexi@proton.me>
2026-05-16 14:57:11 -0300

    testing potentiometer

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+  adc_init();
+  adc_gpio_init(26);
+  adc_select_input(0);
+
+  while (true) {
+    uint16_t raw = adc_read();
+    printf("adc: %u\n", raw);
+    sleep_ms(200);
+  }
 }
